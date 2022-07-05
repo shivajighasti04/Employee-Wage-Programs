6438745eda0d2d2032c8696e52d3d2fb49be8b79
shivajighasti/04 <shivajighasti97@gmail.com>
2022-07-05 22:59:09 +0530

    Employee Wage of multiple companies using Interface

---
+++ CompanyEmpWage.java
@@ -0,0 +1,30 @@
+package com.bridgelabz.employeewagecomp;
+
+public class CompanyEmpWage {
+	
+
+	// Variable Declaration
+	public final String company;
+	public final int empWagePerHr;
+	public final int maxWorkingDays;
+	public final int maxHrPerMonth;
+	public int totalEmpWage;
+
+	// Parameterised constructor
+	public CompanyEmpWage(String company, int empWagePerHr, int workingDayPerMonth, int maxHrsPerMonth) {
+		this.company = company;
+		this.empWagePerHr = empWagePerHr;
+		this.maxWorkingDays = workingDayPerMonth;
+		this.maxHrPerMonth = maxHrsPerMonth;
+	}
+
+	// setter : set total Employee wage
+	public void setTotalEmpWage(int totalEmpWage) {
+		this.totalEmpWage = totalEmpWage;
+	}
+
+	// To-string method: display purpose
+	public String toString() {
+		return "[Monthly wage should paid by " + company + " to emplyee: " + totalEmpWage + "]";
+	}
+}
