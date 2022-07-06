1e650eded736cb19fc1294e45f560397025e6bf5
shivajighasti/04 <shivajighasti97@gmail.com>
2022-07-06 22:07:27 +0530

    Interface for Emp Wage Builder

---
+++ IEmpWageBuilder.java
@@ -0,0 +1,14 @@
+package com.bridgelabz.employeewagecomp;
+
+import java.util.ArrayList;
+
+public interface IEmpWageBuilder {
+	
+	public static final ArrayList<CompanyEmpWage> companyWageArrayList = new ArrayList<CompanyEmpWage>();
+
+	public void addComapanyEmpWage(String company, int empWagePerHr, int maxWorkingDays, int maxHrPerMonth);
+
+	public int empWageCalculation(CompanyEmpWage companyEmpWage);
+
+	public void displayEmpWage();
+}
